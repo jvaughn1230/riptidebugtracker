e244f8fa328424055ad3757d5ad8481ea39284e3
Alex <jvaughn1230@gmail.com>
2023-05-25 06:23:19 -0700

    added basic signup form

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import "./signup.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="signup-page">
+      <div className="signup-container">
+        <h1 className="signup-title">Create A Free Account</h1>
+        <form className="signup-form">
+          <div>
+            <label>First Name: </label>
+            <input type="text" label="firstname" placeholder="First Name...." />
+            <label>Last Name: </label>
+            <input type="text" label="lastname" placeholder="Last Name...." />
+          </div>
+          <div>
+            <label>Username</label>
+            <input
+              type="text"
+              label="username"
+              placeholder="Desired Username...."
+            />
+            <label>Email:</label>
+            <input type="email" label="email" placeholder="Your Email...." />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              label="password"
+              placeholder="password...."
+            />
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              label="password"
+              placeholder="confirm password...."
+            />
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
