840d1ac682e05e0078a49e3db9451775cd46ba36
Alex <jvaughn1230@gmail.com>
2023-05-25 04:57:07 -0700

    started login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Login from "./components/Login/Login";
 
 function App() {
-  return <div>Riptide</div>;
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
