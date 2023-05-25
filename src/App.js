eab168807efa9adca58320e0b3c3d90bfd61f15c
Alex <jvaughn1230@gmail.com>
2023-05-25 05:09:41 -0700

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login/Login";
+import AddBug from "./components/AddBug/AddBug";
+import Signup from "./components/Signup/Signup";
+import ViewBugs from "./components/ViewBugs/ViewBugs";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="app">
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Layout />}>
+          <Route path="/account/addbug" element={<AddBug />} />
+          <Route path="/account/viewbugs" element={<ViewBugs />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/AddBug/AddBug.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddBug = () => {
+  return <div>AddBug</div>;
+};
+
+export default AddBug;
